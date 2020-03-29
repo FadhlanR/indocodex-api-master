b7cb9b6b570e98729f1f8a9f250cf5331abca0e1
Fadhlan Ridhwanallah <fridhwanallah@gmail.com>
2020-03-30 00:13:30 +0700

    Add

---
+++ setEnvironment.sh
@@ -0,0 +1,4 @@
+# Copy this file to your own setEnvironment.sh and edit the configs to your own bucket
+export STELLARTERM_S3_BUCKET="api.stellarterm.com"
+export STELLARTERM_AWS_PROFILE="stellarterm"
+export COIN_MARKET_CUP_KEY='cb7ebafb-322d-460b-837b-97ca2126786f'
